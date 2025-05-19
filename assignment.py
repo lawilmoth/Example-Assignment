06ea7377c1a6643e80a47645612c1f9abdc7ab56
mrw <lwilmoth@eriesd.org>
2025-05-19 08:43:47 -0400

    fixed return type

--- assignment.py
+++ assignment.py
@@ -6,12 +6,12 @@ def return_hello():
     pass
 
 
-def return_hello_person_name(name:str) -> None:
+def return_hello_person_name(name:str) -> str:
     """Returns 'Hello, <name>!' to the console.
     """
     pass
 
-def return_hello_person_name_age(name:str, age:int) -> None:
+def return_hello_person_name_age(name:str, age:int) -> str:
     """Returns 'Hello, <name>! You are <age> years old. On your birthday, you will be <age +1>.' to the console.
     """
     pass
